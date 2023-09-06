282d1a00a4cfbf4b8299057aff784d72749dfe72
Nitish <nitishjsr7209@gmail.com>
2023-09-06 15:56:02 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
+"use client"
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/Components/Navbar/navbar'
+import {Provider} from 'react-redux'
+import {store} from '@/store/store'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body suppressHydrationWarning >
+        <Provider store={store} >
+        <Navbar/>
+        {children}
+        </Provider>
+        </body>
     </html>
   )
 }
